6c34e9b4b497a1797980be939bcd5b77fee85e89
natbutter <natbutter@gmail.com>
2025-11-23 18:07:08 +1100

    updates

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,5 +3,5 @@ from .api import app
 from .config import BACKEND_HOST, BACKEND_PORT
 
 if __name__ == "__main__":
-    uvicorn.run("backend.app.api:app", host=BACKEND_HOST, port=BACKEND_PORT, log_level="info")
+     uvicorn.run("app.api:app", host=BACKEND_HOST, port=BACKEND_PORT, log_level="info")
 
--- backend/app/run_uvicorn.sh
+++ backend/app/run_uvicorn.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-exec gunicorn -k uvicorn.workers.UvicornWorker backend.app.api:app -b 0.0.0.0:8000 --workers 1 --timeout 120
-
+exec gunicorn -k uvicorn.workers.UvicornWorker app.api:app -b 0.0.0.0:8000 --workers 1 --timeout 120
